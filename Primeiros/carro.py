ddb652068bd10953660046c5f82c337f0c4a18a5
Vinicioscastro <61066046+Vinicioscastro@users.noreply.github.com>
2022-04-15 23:52:42 -0300

    Update carro.py

--- Primeiros/carro.py
+++ Primeiros/carro.py
@@ -3,3 +3,5 @@ d= float(input("Quantos dias: "))
 km = float(input('quantos Km: '))
 
 print('Valor a pagar por {} dias é {} e por rodar {} quilometros e {} assim com um total de {}: '.format(d, (d*60), km, (km*0.15), ((d*60)+(km*0.15))))
+
+
